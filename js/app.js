b8026e1b10ea965ba71e9699594b7fbd80cdda58
Aibek <aibek.abdykasymov@gmail.com>
2017-02-15 14:38:30 +0600

    Add simple components

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
 console.log(React);
 console.log(ReactDOM);
 
+var App = React.createClass({
+    render: function () {
+        return (
+            <div className="app">
+                Всем привет, я компоненет App
+                <News/>
+                <Comments/>
+            </div>
+        );
+    }
+});
+
+var News = React.createClass({
+    render: function () {
+        return (
+            <div className="news">
+                К сожалению нет новостей
+            </div>
+        );
+    }
+});
+
+var Comments = React.createClass({
+    render: function () {
+        return (
+            <div className="comments">
+                Нет новостей комментировать нечего
+            </div>
+        );
+    }
+});
+
 ReactDOM.render(
-    <h1>Hello, world! </h1>,
+    <App/>,
     document.getElementById('root')
 );
