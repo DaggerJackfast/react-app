eb2a3a5efd5ad81ef2843518e63e2b2c0a198813
Aibek <aibek.abdykasymov@gmail.com>
2017-02-15 15:06:58 +0600

    Use props in components

--- js/app.js
+++ js/app.js
@@ -1,23 +1,57 @@
 console.log(React);
 console.log(ReactDOM);
 
+var my_news = [
+    {
+        author: 'Author 1',
+        text: 'Text 1'
+    },
+    {
+        author: 'Author 2',
+        text: 'Text 2'
+    },
+    {
+        author: 'Author 3',
+        text: 'Text 3'
+    },
+    {
+        author: 'Author 4',
+        text: 'Text 4'
+    },
+    {
+        author: 'Author 5',
+        text: 'Text 5'
+    },
+];
+
 var App = React.createClass({
     render: function () {
         return (
             <div className="app">
-                Всем привет, я компоненет App
-                <News/>
+                Всем привет, я обновленнный компоненет App
+                <News data={my_news}/>{/* новое свойство */}
                 <Comments/>
             </div>
         );
     }
 });
 
+
 var News = React.createClass({
     render: function () {
+        var data = this.props.data;
+        var newsTemplate = data.map(function (item, index) {
+            return (
+                <div key={index}>
+                    <p className="news__author">{item.author}</p>
+                    <p className="news__text">{item.text}</p>
+                </div>
+            )
+        });
+        console.log(newsTemplate);
         return (
             <div className="news">
-                К сожалению нет новостей
+                {newsTemplate}
             </div>
         );
     }
